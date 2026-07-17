3a35f3ccd0bb209df98aa6141eb4d334df035384
navid.nasrollahi@yahoo.com <Nasr6620514>
2026-07-17 09:04:29 +0330

    Create accounts app

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
